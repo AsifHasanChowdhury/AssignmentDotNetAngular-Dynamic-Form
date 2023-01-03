f1c4be0194596da3501f324864fd5f2b673555e2
Mehedi Hasan Bhuiyan <82281652+mhb98@users.noreply.github.com>
2023-01-03 12:08:58 +0600

    UI Update

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -16,6 +16,9 @@ export class AppComponent implements OnInit {
   htmlview: SafeHtml | undefined;
   genders = ['Male', 'Female'];
 
+    
+
+
 
   constructor(private http: HttpClient,private sanitizer:DomSanitizer,private jwtHelper: JwtHelperService) {}
 
@@ -29,46 +32,6 @@ export class AppComponent implements OnInit {
 
     }
 
-  // onFetchPosts() {
-  //   // Send Http request
-  //   this.fetchPosts();
-  // }
-
-
-  /*
-  private fetchPosts() {
-    this.http
-      .get('https://localhost:44396/api/ProductAPI/SendFormModule')
-      .pipe(
-        map(responseData => {
-
-          this.alphas=responseData as string
-          this.htmlview=this.sanitizer.bypassSecurityTrustHtml(this.alphas);
-          //console.log(this.alphas)
-
-        })
-      )
-      .subscribe(posts => {
-        // ...
-        console.log("Hello");
-      });
-  }
-
-  onSubmit(form:HTMLFormElement){
-
-    this.http
-      .post(
-        'https://localhost:44396/api/ProductAPI/GetFormResponse',
-        form['value']
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
-    console.log(form['value']);
-  }
-
-
-   */
   isUserAuthenticated = (): boolean => {
     const token = localStorage.getItem("jwt");
 
